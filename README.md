932583a9b300627f8e886a16f1bb39e92e968b63
T.Suzuki <suzuki_t@hdks.co.jp>
2019-08-20 11:04:54 +0900

    [add]first

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # QuartusCLI
-Quartus command line tool on WSL
+
+Quartus command line tool on WSL. This script works in WSL bash. 3 different Quartus tools are executed by the argument.
+
+1. Quartus GUI launch(argument is none or project name)
+2. Compilation(argument is `sh`)
+3. Simulation(argument is `sim`)
+
+### e.g.
+
+#### 1.
+
+```bash
+$ quartus.sh
+```
+
+Quartus GUI is launch.
+
+```bash
+$ quartus.sh PROJECT
+```
+
+Quartus GUI is launch and PROJECT.qpf opens.
+
+#### 2.
+
+```bash
+$ quartus.sh sh
+Info: *******************************************************************
+Info: Running Quartus Prime Shell
+~
+Info: Command: quartus_sh --flow compile PROJECT
+Info: Quartus(args): compile PROJECT
+~
+```
+
+Compilation of current directory project starts.
+
+```bash
+$ quartus.sh sh PROJECT
+Info: *******************************************************************
+Info: Running Quartus Prime Shell
+~
+Info: Command: quartus_sh --flow compile PROJECT
+Info: Quartus(args): compile PROJECT
+~
+```
+
+Compilation of project that is specified by the argument starts.
+
+#### 3.
+
+It works similarly argument `sh`.
+
+### note
+
+`$quaruts_bin` specifies Quartus installation directory. e.g. Quartus Prime 18.0 is `intelFPGA_lite/18.0/quartus/bin64`.
+
+## License
+
+MIT License
+
+## Author
+
+[toms74209200](<https://github.com/toms74209200>)
---
+++ quartus.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#------------------------------------------------------------------------------
+# quartus
+# Quartus command line tool on WSL
+# 
+# Copyright (c) 2019 toms74209200
+# 
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
+#------------------------------------------------------------------------------
+
+quartus_bin="<Quartus installation directory>"
+
+if [ $# = 0 ]; then
+    if find -name *.qpf; then
+        find -name *.qpf -printf '%f\n' | sed 's/\.[^\.]*$//' | xargs -I% $quartus_bin/quartus.exe % &
+    else
+        $quartus_bin/quartus.exe &
+    fi
+elif [ $# = 1 ]; then
+    if [ $1 = "sh" ]; then
+        if [ -f *.qpf ]; then
+            find -name *.qpf -printf '%f\n' | sed 's/\.[^\.]*$//' | xargs -I% $quartus_bin/quartus_sh.exe --flow compile %
+        else
+            echo "Project is not found."
+        fi
+    elif [ $1 = "sim" ]; then
+        if [ -f *.qpf ]; then
+            find -name *.qpf -printf '%f\n' | sed 's/\.[^\.]*$//' | xargs -I% $quartus_bin/quartus_sim.exe %
+        else
+            echo "Project is not found."
+        fi
+    else
+        if [ -f $1.qpf ]; then
+            $quartus_bin/quartus.exe $1 &
+        else
+            echo "Project is not found."
+        fi
+    fi
+elif [ $# = 2 ]; then
+    if [ $1 = "sh" ]; then
+        if [ -f $2.qpf ]; then
+            $quartus_bin/quartus_sh.exe --flow compile $2
+        else
+            echo "Project is not found."
+        fi
+    elif [ $1 = "sim" ]; then
+        if [ -f $2.qpf ]; then
+            $quartus_bin/quartus_sim.exe $2
+        else
+            echo "Project is not found."
+        fi
+    else
+        echo "Invalid argument"
+    fi
+fi
