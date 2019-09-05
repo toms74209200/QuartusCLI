1776b9598eff6e296cf8410c352513769a3150b8
T.Suzuki <suzuki_t@hdks.co.jp>
2019-09-05 14:25:49 +0900

    [add]add make project and archive project

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # QuartusCLI
 
-Quartus command line tool on WSL. This script works in WSL bash. 3 different Quartus tools are executed by the argument.
+Quartus command line tool on WSL. This script works in WSL bash. 5 different Quartus tools are executed by the argument.
 
 1. Quartus GUI launch(argument is none or project name)
-2. Compilation(argument is `sh`)
-3. Simulation(argument is `sim`)
+1. Compilation(argument is `sh`)
+1. Simulation(argument is `sim`)
+1. Make project(argument is `mk`)
+1. Archive project(argument is `qar`)
 
 ### e.g.
 
@@ -51,6 +53,36 @@ Compilation of project that is specified by the argument starts.
 #### 3.
 
 It works similarly argument `sh`.
+
+#### 4.
+
+```bash
+$ quartus.sh mk PROJECT
+```
+
+Quartus project is made. Source files(VHDL`.vhd`, Verilog HDL`.v`, Block Diagram/Schematic File`.bdf`) are added in project.
+
+#### 5.
+
+```bash
+$ quartus.sh qar REV
+Info: *******************************************************************
+Info: Running Quartus Prime Shell
+~
+Info: Command: quartus_sh --archive -output ./archive/PROJECT_REV_YYMMDD/PROJECT_REV_YYMMDD PROJECT
+Info: Quartus(args): -qar -output ./archive/PROJECT_REV_YYMMDD/PROJECT_REV_YYMMDD PROJECT
+```
+
+Archive project. `.qar` file is put in `./archive` directory. The below source files list are copied in `./archive/src` directory.
+
+- VHDL(`.vhd`)
+- Verilog HDL(`.v`)
+- Block Diagram/Schematic File(`.bdf`)
+- Block Symbol FIle(`bsf`)
+- Quartus IP file(`.qip`)
+- Qsys design file(`.qsys`)
+
+ Programing files(`.sof`, `.pof`, `.jic`) are also copied `./archive/output_files` directory.
 
 ### note
 
--- quartus.sh
+++ quartus.sh
@@ -11,9 +11,33 @@
 
 quartus_bin="<Quartus installation directory>"
 
+# Function ####################################################################
+
+function find_project() {
+    project=$(find -name "*.qpf" -printf '%f\n' | sed 's/\.[^\.]*$//' | head -1)
+}
+
+function make_archive() {
+    mkdir -p ./archive/$qar_dir
+    $quartus_bin/quartus_sh.exe --archive -output ./archive/$qar_dir/$qar_dir $project
+    mkdir -p ./archive/$qar_dir/src
+    find -name "*.vhd"  | xargs -I% cp % ./archive/$qar_dir/src/
+    find -name "*.v"    | xargs -I% cp % ./archive/$qar_dir/src/
+    find -name "*.bdf"  | xargs -I% cp % ./archive/$qar_dir/src/
+    find -name "*.bsf"  | xargs -I% cp % ./archive/$qar_dir/src/
+    find -name "*.qip"  | xargs -I% cp % ./archive/$qar_dir/src/
+    find -name "*.qsys" | xargs -I% cp % ./archive/$qar_dir/src/
+    mkdir -p ./archive/$qar_dir/program_files
+    find -name "*.sof" | xargs -I% cp % ./archive/$qar_dir/program_files/${project}.sof
+    find -name "*.pof" | xargs -I% cp % ./archive/$qar_dir/program_files/${project}.sof
+    find -name "*.jic" | xargs -I% cp % ./archive/$qar_dir/program_files/${project}.sof
+}
+
+# Main script #################################################################
+
 if [ $# = 0 ]; then
-    if [ -f  *.qpf ]; then
-        project=$(find -name "*.qpf" -printf '%f\n' | sed 's/\.[^\.]*$//' | head -1)
+    if [ -f *.qpf ]; then
+        find_project
         $quartus_bin/quartus.exe $project &
     else
         $quartus_bin/quartus.exe &
@@ -21,18 +45,27 @@ if [ $# = 0 ]; then
 elif [ $# = 1 ]; then
     if [ $1 = "sh" ]; then
         if [ -f  *.qpf ]; then
-            project=$(find -name "*.qpf" -printf '%f\n' | sed 's/\.[^\.]*$//' | head -1)
+            find_project
             $quartus_bin/quartus_sh.exe --flow compile $project
         else
             echo "Project is not found."
         fi
     elif [ $1 = "sim" ]; then
         if [ -f  *.qpf ]; then
-            project=$(find -name "*.qpf" -printf '%f\n' | sed 's/\.[^\.]*$//' | head -1)
+            find_project
             $quartus_bin/quartus_sim.exe $project
         else
             echo "Project is not found."
         fi
+    elif [ $1 = "qar" ]; then
+        if [ -f  *.qpf ]; then
+            find_project
+            today=$(date "+%y%m%d")
+            qar_dir="${project}_${today}"
+            make_archive
+        fi
+    elif [ $1 = "mk" ]; then
+        echo "Project name is unspecified."
     else
         if [ -f $1.qpf ]; then
             $quartus_bin/quartus.exe $1 &
@@ -53,7 +86,27 @@ elif [ $# = 2 ]; then
         else
             echo "Project is not found."
         fi
+    elif [ $1 = "qar" ]; then
+        if [ -f $2.qpf ]; then
+            project="$2"
+            today=$(date "+%y%m%d")
+            qar_dir="$2_${today}"
+            make_archive
+        else
+            find_project
+            today=$(date "+%y%m%d")
+            qar_dir="${project}_$2_${today}"
+            make_archive
+        fi
+    elif [ $1 = "mk" ]; then
+        $quartus_bin/quartus_sh.exe --tcl_eval project_new $2
+        echo >> $2.qsf
+        echo set_global_assignment -name PROJECT_OUTPUT_DIRECTORY output_files >> $2.qsf
+        find -name "*.vhd" | xargs -I% echo set_global_assignment -name VHDL_FILE % >> $2.qsf
+        find -name "*.v"   | xargs -I% echo set_global_assignment -name VERILOG_FILE % >> $2.qsf
+        find -name "*.bdf" | xargs -I% echo set_global_assignment -name BDF_FILE % >> $2.qsf
     else
         echo "Invalid argument"
     fi
 fi
+
